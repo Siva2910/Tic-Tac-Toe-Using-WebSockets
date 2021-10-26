2eeb983a54a91a1fa41f80bbd82bbaa57ee38e8e
Siva Kumar <67836160+Siva2910@users.noreply.github.com>
2021-10-26 14:05:54 +0530

    Code

---
+++ webServer.js
@@ -0,0 +1,70 @@
+import express from 'express';
+var app = express()
+const port = process.env.PORT||7000;
+import {Server} from 'socket.io'
+
+var server=app.listen(port,()=>{
+    console.log("Listening to request on port "+port)
+})
+
+app.use(express.static('public'))
+
+var io= new  Server(server);
+
+
+var players = {};
+var unmatched;
+
+
+io.on("connection", function (socket) {
+    console.log("socket connected")
+  socket.emit('connect1',{msg:"hello"})
+  joinGame(socket);
+
+  if (getOpponent(socket)) {
+    socket.emit("game.begin", {
+      symbol: players[socket.id].symbol,
+    });
+    getOpponent(socket).emit("game.begin", {
+      symbol: players[getOpponent(socket).id].symbol,
+    });
+  }
+
+  socket.on("make.move", function (data) {
+    if (!getOpponent(socket)) {
+      return;
+    }
+    socket.emit("move.made", data);
+    getOpponent(socket).emit("move.made", data);
+  });
+
+  socket.on("disconnect", function () {
+    if (getOpponent(socket)) {
+      getOpponent(socket).emit("opponent.left");
+    }
+  });
+});
+
+function joinGame(socket) {
+  players[socket.id] = {
+    opponent: unmatched,
+
+    symbol: "X",
+    // The socket that is associated with this player
+    socket: socket,
+  };
+  if (unmatched) {
+    players[socket.id].symbol = "O";
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  } else {
+    unmatched = socket.id;
+  }
+}
+
+function getOpponent(socket) {
+  if (!players[socket.id].opponent) {
+    return;
+  }
+  return players[players[socket.id].opponent].socket;
+}
